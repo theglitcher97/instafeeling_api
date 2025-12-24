0114cb901d169ab3f50b7dda6196a89923ea59f5
Fernando Orozco <forozcomercado@gmail.com>
2025-12-23 20:34:08 -0500

    initial commit

---
+++ src/main/java/com/instafeeling/InstafeelingApplication.java
@@ -0,0 +1,13 @@
+package com.instafeeling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstafeelingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstafeelingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/instafeeling/InstafeelingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instafeeling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstafeelingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
