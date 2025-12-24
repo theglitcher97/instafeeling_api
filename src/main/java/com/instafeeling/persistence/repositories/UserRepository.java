f94312791e22e09a2ae29641ec331cf0c93d68a3
Fernando Orozco <forozcomercado@gmail.com>
2025-12-23 21:22:08 -0500

    feat: add UserRepository

---
+++ src/main/java/com/instafeeling/persistence/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.instafeeling.persistence.repositories;
+
+
+import com.instafeeling.persistence.entities.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
+}
