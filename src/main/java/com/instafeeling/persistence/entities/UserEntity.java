0183a7d45bdb6a11f8b880b96b3e5a9123fd0bf6
Fernando Orozco <forozcomercado@gmail.com>
2025-12-23 21:18:25 -0500

    feat: UserEntity initial setup

---
+++ src/main/java/com/instafeeling/persistence/entities/UserEntity.java
@@ -0,0 +1,25 @@
+package com.instafeeling.persistence.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
