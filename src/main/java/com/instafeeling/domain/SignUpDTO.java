00b9eb6f98fb8323c06f19f65bfa3ff41097edef
Fernando Orozco <forozcomercado@gmail.com>
2025-12-23 22:25:36 -0500

    feat: add SignUpDTO

---
+++ src/main/java/com/instafeeling/domain/SignUpDTO.java
@@ -0,0 +1,19 @@
+package com.instafeeling.domain;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record SignUpDTO(
+        @NotBlank(message = "please provide an email") // assert not null and not empty
+        @Email(message = "please provide a valid email")
+        String email,
+
+        @NotBlank(message = "please provide a password")
+        @Size(min = 8, max = 255, message = "the password must be between 8 and 255 characters long")
+        String password,
+
+        @NotBlank(message = "please provide a confirmation password")
+        String confirmPassword
+) {
+}
