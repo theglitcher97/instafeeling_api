adede6244e9d3ed41d2f536da4a286013fc64af1
fernando <forozcomercado@gmail.com>
2026-02-21 11:07:41 -0500

    Feat: mark all notifications as readd Ref: add notification type to ContentLikedEvent

--- src/main/java/com/instafeeling/application/events/ContentLikedEvent.java
+++ src/main/java/com/instafeeling/application/events/ContentLikedEvent.java
@@ -2,7 +2,6 @@ package com.instafeeling.application.events;
 
 import com.instafeeling.application.notifications.NotificationType;
 
-//TODO:  Add Notification Type
 public record ContentLikedEvent(
         Long actorId,
         Long contentId,
